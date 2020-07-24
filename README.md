cb480f5e72ffd5e36f42c599adff0eca8a6429a7
Ziyad Mestour <ziyad.mestour@gmail.com>
2020-07-24 11:46:50 +0200

    Add files

---
+++ README.md
@@ -0,0 +1,19 @@
+# Automate Downloading Navigo Certificate
+
+## How it works ?
+* It is exhausting to download every month a navigo (paris public transportation pass subscription) to send it to your employer. This basic script downloads it and renames to a convenient name so that you don't have to do it manually.
+
+## Configuration
+* There are very few things to configure in the script, these are commented in the py scirpt. Nevetherless
+	* Your **email** address used to subscribe to Navigo
+	* **Password** encoded in **base64**
+	* Your **first name** and **last name**
+	* **Default path of your downloads**
+	* Optional path to move file from **Downloads** to another custom path
+	
+	
+## Contribution
+* Feel free to fork or submit a PR.
+
+## Contact
+email: ziyad.mestour@gmail.com
---
+++ main.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import webbot
+from base64 import b64decode
+from os import rename
+from datetime import datetime
+from calendar import monthrange
+import time
+from shutil import move
+
+
+def get_month_and_year(date, month_dict):
+    month_number = date.month
+    month_name = month_dict[month_number]
+    return month_name, date.year
+
+
+def get_monthly_subscription_certificate(date, month_dict, web, url, username,
+                                         password):
+    month, year = get_month_and_year(date, month_dict)
+    number_of_days_in_month = monthrange(year, date.month)[1]
+
+    # Login and navigate to certificate download page.
+    web.go_to(url)
+    web.type(username, into="username")
+    web.type(b64decode(password).decode("utf-8"), into="Password",
+             id="email-connect-error-dialog")
+    web.click("Me Connecter", tag="span")
+    web.click(id="compte-user-mon-espace-a-loop-1", tag="a")
+    web.click("Mon attestation", tag="span")
+
+    # Set start Date and year
+    web.click(id="attestation_moisDebut", tag="select")
+    web.click("1er {}".format(month),  tag="option")
+    web.click(id="attestation_moisDebut", tag="select")
+    web.click(id="attestation_anneeDebut", tag="select")
+    web.click(str(year), tag="option")
+
+    # Set end Date and year
+    web.click(id="attestaition_moisFin", tag="select")
+    web.click("{} {}".format(number_of_days_in_month, month),  tag="option")
+    web.click(id="attestation_anneeFin", tag="select")
+    web.click(str(year), tag="option")
+
+    # Download
+    web.click(id="actes-payment-attestation-txt-5", tag="button")
+    time.sleep(5)
+
+    web.driver.maximize_window()
+    # Logout
+    web.click(id="sel-base-nav-27", tag="a")
+    web.quit()
+    # web.click("Déconnexion", tag="span")
+
+    return 0
+
+
+def rename_pdf(file_path, month, year, first_name, last_name):
+    rename(file_path + "/attestation.pdf", file_path +
+           "attestation_navigo_{}_{}_{}_{}".format(month, year,
+                                                   first_name, last_name))
+
+
+def move_pdf(file_path, month, year, first_name, last_name, new_file_path=""):
+    if new_file_path != "":
+        move(file_path + "attestation_navigo_{}_{}_{}_{}"
+             .format(month, year, first_name, last_name),
+             new_file_path + "attestation_navigo_{}_{}_{}_{}"
+             .format(month, year, first_name, last_name))
+
+
+if __name__ == "__main__":
+    date = datetime.now()
+    web = webbot.Browser()
+    navigo_url = "https://www.jegeremacartenavigo.fr/connexion/connexion"
+    username = "email@gmail.com"
+    # base64 encoded password
+    password = "XlNsYXNodGVSDFD234"
+    first_name = "first_name"
+    last_name = "last_name"
+    # Default path where files are downloaded in your PC
+    file_path = "/home/file/path"
+    # Optional path to move file tobelow
+    new_file_path = ""
+
+    month_number_name = {
+        1: "janvier",
+        2: "février",
+        3: "mars",
+        4: "avril",
+        5: "mai",
+        6: "juin",
+        7: "juillet",
+        8: "aout",
+        9: "septembre",
+        10: "octobre",
+        11: "novembre",
+        12: "décembre"
+    }
+
+    # Functions call.
+    month, year = get_month_and_year(date, month_number_name)
+    get_monthly_subscription_certificate(date, month_number_name, web,
+                                         navigo_url, username, password)
+    rename_pdf(file_path, month, date.year, first_name, last_name)
+    move_pdf(file_path, month, date.year, first_name, last_name,
+             new_file_path)
